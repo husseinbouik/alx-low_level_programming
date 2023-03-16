78847afa356798735a10c4535eedbd4bfdc1c254
husseinbouik <husseinbouik5@gmail.com>
2023-03-16 21:29:16 +0100

    programmation c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - 101_quote
+ * Return: 1
+ */
 
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
-
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
-}
+/**
+ * main - print a string without using printf only with puts
+ * Return: 0
+ */
 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
